deb80a8c6bc342b9e6fb4121ac5cd37180f0abd4
manalikedar <32631593+manalikedar@users.noreply.github.com>
2024-07-18 16:17:43 +0100

    Add files via upload

---
+++ OpenGLCourseApp_Triangle/OpenGLCourseApp/OpenGLCourseApp/DirectionalLight.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Light.h"
+#include "glm.hpp"
+class DirectionalLight :
+    public Light
+{
+public:
+    DirectionalLight();
+    DirectionalLight(GLuint shadowWidth, GLuint shadowHeight,
+        GLfloat red, GLfloat  green, GLfloat blue,
+        GLfloat aIntensity, GLfloat dIntensity,
+        GLfloat xDir, GLfloat yDir, GLfloat zDir);
+
+    void UseLight(GLfloat ambientIntensityLocation, GLfloat ambientColorLocation,
+        GLfloat diffuseIntensityLocation, GLfloat directionLocation);
+
+    glm::mat4 CalculateLightTransform();
+
+    ~DirectionalLight();
+
+private:
+    glm::vec3 direction;
+};
+
